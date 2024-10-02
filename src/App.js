53e98d780e13287b7f86590321c8d7e37135019b
Prad-Nanduri <84355939+Prad-Nanduri@users.noreply.github.com>
2024-10-01 20:55:19 -0400

    first

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Let's Play Pradle - Wordle without any limits. </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to Pradle!
         </a>
       </header>
     </div>
