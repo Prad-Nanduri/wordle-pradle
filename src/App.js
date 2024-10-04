b17d5ddf32b15e40aba65f924d16591d1aa50196
Prad-Nanduri <84355939+Prad-Nanduri@users.noreply.github.com>
2024-10-03 23:24:27 -0400

    state-edit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import './app.css';
 import Header from './components/header.js';
 import Grid from './components/grid.js';
 import Keyboard from './components/keyboard.js';
+import { useState } from 'react';
 
 function App() {
+  const All_English_Words = ["MATCH", "CATCH", "TOUCH", ]
+  const [mysteryWord, setMysteryWord] = useState("CATCH")
+  const [currentRow, setCurrentRow] = useState(0)
+  const [currentWord, setCurrentWord] = useState("")
+  const [guessedWords, setGuessedWords] = useState([])
   const width = 5
   const height = 6
+  // const getContent() {
+
+  //   return [
+  //     []
+  //   ]
+  // }
 
 
   return (
     <div className="app-container">
      <Header />
-     <Grid width={width} height={height} />
+     <Grid 
+        width={width}
+        height={height}
+        content={getContent()}
+        />
      <Keyboard />
 
     </div>                                                                                                            
