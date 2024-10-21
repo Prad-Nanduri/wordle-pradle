ece6792712b736a8b88130c1fe68ece4b878bba1
Prad-Nanduri <84355939+Prad-Nanduri@users.noreply.github.com>
2024-10-20 22:23:11 -0400

    states p1

--- src/App.js
+++ src/App.js
@@ -6,18 +6,58 @@ import { useState } from 'react';
 
 function App() {
   const All_English_Words = ["MATCH", "CATCH", "TOUCH", ]
+  // const [mysteryWord, setMysteryWord] = useState("CATCH")
+  // const [currentRow, setCurrentRow] = useState(0)
+  // const [currentWord, setCurrentWord] = useState("")
+  // const [guessedWords, setGuessedWords] = useState([])
+
   const [mysteryWord, setMysteryWord] = useState("CATCH")
   const [currentRow, setCurrentRow] = useState(0)
-  const [currentWord, setCurrentWord] = useState("")
-  const [guessedWords, setGuessedWords] = useState([])
+  const [currentWord, setCurrentWord] = useState("TOU")
+  const [guessedWords, setGuessedWords] = useState(["PATCH"])
   const width = 5
   const height = 6
-  // const getContent() {
 
-  //   return [
-  //     []
-  //   ]
-  // }
+  const getContent = () => {
+
+    // guessedWords.forEach
+
+    guessedWords.each((guessedWord, rowNum) => {
+      for (let guessedWordIndex = 0; guessedWordIndex < guessedWord.length; guessedWordIndex++){
+        const gridRowNum = rowNum
+        const gridColumnNum = guessedWordIndex
+        const character = guessedWord[guessedWordIndex]
+        let color;
+         if (!mysteryWord.includes(character)) {
+          color = "grey"
+        } else if (mysteryWord[guessedWordIndex] == character) {
+          color = 'green'
+        }else {
+          color = 'yellow'
+        } 
+        }
+        //letter in the AnswerWord = > GREY
+        //Letter in the AnswerWord but different position
+      }
+    })
+
+    return {
+      
+        "0,0": {
+          color: "green",
+          text: "T"
+        },
+        "0,1":{
+          color:"grey",
+          text:"O"
+        },
+        "0,2":{
+          color:"grey",
+          text:"U"
+        },
+      
+      }
+  }
 
 
   return (
--- src/components/grid.js
+++ src/components/grid.js
@@ -1,10 +1,27 @@
 import "./grid.scss";
 
 export default function Grid(props) {
-    const tiles = [...Array(props.height).keys()].map((i, _) =>
-        <div className="row" key={`${i}`}>
-            {[...Array(props.width).keys()].map((j, _) => {
-            return (<div className="tile" key={`${i}-${j}`}>{`(${i}, ${j})`}</div>);
+    const tiles = [...Array(props.height).keys()].map((rowNum, _) =>
+        <div className="row" key={`${rowNum}`}>
+            {[...Array(props.width).keys()].map((columnNum, _) => {
+                const content = props.content[`${rowNum},${columnNum}`]
+                if (content){
+                    return(
+                        <div
+                        className={`tile background-${content["color"]}`}
+                        key = {`${rowNum}-${columnNum}`}>
+                        {content["text"]}
+                        </div>
+                    )
+                } else{
+                    return(
+                        <div
+                        className="tile"
+                        key = {`${rowNum}, ${columnNum}`}>
+                        </div>
+                    )
+                }
+            // return (<div className="tile" key={`${rowNum}-${columnNum}`}>{`(${rowNum}, ${columnNum})`}</div>);
         })} 
         </div>
     )
